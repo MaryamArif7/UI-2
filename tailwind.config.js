66e31cae5a4a7ce865c1787255700013c2f581ea
MaryamArif7 <www.maryamarif28242@gmail.com>
2025-01-10 14:40:12 +0500

    started

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./public/index.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+     
+      },
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],  // The font-poppins class
+      }
+    },
+  },
+  plugins: [],
+}
